b56568f934b977c4626899a17a53474709a83f8f
TomasAlegriaMTZ <thalberto04@gmail.com>
2025-08-06 14:30:43 -0600

    Create oop version of cart

--- js/checkout.js
+++ js/checkout.js
@@ -1,5 +1,6 @@
 import { renderOrderSummary, headerCartQuantity } from "./checkout/orderSummary.js";
-import {renderPaymentSummary} from './checkout/paymentSummary.js'
+import {renderPaymentSummary} from './checkout/paymentSummary.js';
+import '../data/cart-oop.js';
 
 renderOrderSummary();
 headerCartQuantity(); // Update the cart quantity in the header
